501e48e79dfc94d677bc0bbb01b69b06123dd118
Anirudh <anirudhece2020@gmail.com>
2023-06-06 11:05:55 -0700

    resolved an issue in add (inside slice)

--- src/store/slices/InvoiceSliceReducer.jsx
+++ src/store/slices/InvoiceSliceReducer.jsx
@@ -1,42 +1,42 @@
 import { createSlice } from "@reduxjs/toolkit";
-const InvoiceSlice=createSlice({
-    name:'invoiceSlice',
+const InvoiceSlice = createSlice({
+    name: 'invoiceSlice',
     // for now im not making initialState state as an array but i believe this state must be kept inside an array  
-    initialState: 
-        {
-            isOpen: false,
-            currency: '$',
-            currentDate: '',
-            invoiceNumber: 1,
-            dateOfIssue: '',
-            billTo: '',
-            billToEmail: '',
-            billToAddress: '',
-            billFrom: '',
-            billFromEmail: '',
-            billFromAddress: '',
-            notes: '',
-            total: '0.00',
-            subTotal: '0.00',
-            taxRate: '',
-            taxAmount: '0.00',
-            discountRate: '',
-            discountAmount: '0.00',
-            items: [
-                {
-                    id: 0,
-                    name: '',
-                    description: '',
-                    price: '1.00',
-                    quantity: 1
-                }
-            ]
-        }
+    initialState:
+    {
+        isOpen: false,
+        currency: '$',
+        currentDate: '',
+        invoiceNumber: 1,
+        dateOfIssue: '',
+        billTo: '',
+        billToEmail: '',
+        billToAddress: '',
+        billFrom: '',
+        billFromEmail: '',
+        billFromAddress: '',
+        notes: '',
+        total: '0.00',
+        subTotal: '0.00',
+        taxRate: '',
+        taxAmount: '0.00',
+        discountRate: '',
+        discountAmount: '0.00',
+        items: [
+            {
+                id: 0,
+                name: '',
+                description: '',
+                price: '1.00',
+                quantity: 1
+            }
+        ]
+    }
     ,
-    reducers:{
-        add(state,action){
-            state.push(action.payload)
-        }
+    reducers: {
+        add: (state, action) => {
+            state.items.push(action.payload);
+        },
     }
 })
 export const { add } = InvoiceSlice.actions;
